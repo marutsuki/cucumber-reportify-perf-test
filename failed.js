541e868e5cca34337893ab3fd49ef398fb44a983
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-08-05 07:47:26 +1000

    new site

--- failed.js
+++ failed.js
@@ -2,4 +2,4 @@
                 window.failed = {}; 
                 window.failed.providers = []; 
                 window.failed.providers.push(
-                () => fetch('failed-0.json').then(res => res.json()),);window.failed.pages = 1;
+                () => fetch('failed-0.json').then(res => res.json()),() => fetch('failed-1.json').then(res => res.json()),() => fetch('failed-2.json').then(res => res.json()),() => fetch('failed-3.json').then(res => res.json()),() => fetch('failed-4.json').then(res => res.json()),() => fetch('failed-5.json').then(res => res.json()),);window.failed.pages = 534;
